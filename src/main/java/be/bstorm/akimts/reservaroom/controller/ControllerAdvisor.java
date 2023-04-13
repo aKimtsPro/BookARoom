b20d23d1ae989f4872bae9ff90d883033670fbb5
Alexandre Kimtsaris <alexandre.kimtsaris@bstorm.be>
2023-04-13 16:46:47 +0200

    init

--- src/main/java/be/bstorm/akimts/reservaroom/controller/ControllerAdvisor.java
+++ src/main/java/be/bstorm/akimts/reservaroom/controller/ControllerAdvisor.java
@@ -1,10 +1,5 @@
 package be.bstorm.akimts.reservaroom.controller;
 
-import be.bstorm.akimts.reservaroom.models.dto.ErrorDTO;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.ErrorResponseException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
